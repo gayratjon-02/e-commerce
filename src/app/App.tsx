3e7b4b58fdf3c80eed0f7f2549aaae9edea47677
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-08-10 05:26:47 +0900

    feat: MUI standard integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "../css/app.css";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import {Box, Button, Container, Stack, Typography} from "@mui/material";
+
 
 function App() {
-  return <div>HOME</div>;
+    return <Container maxWidth={"sm"}>
+        <Stack flexDirection={"column"}>
+            <Box sx={{my: 4}}>
+                <Typography variant={'h4'} component={'h4'}>Create React app with Redux and TypeScript</Typography>
+            </Box>
+        </Stack>
+    </Container>
 }
-
-export default App;
+    export default App;
 
 
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { store } from "./app/store";
+import {Provider} from "react-redux";
+import {store} from "./app/store";
 import App from "./app/App";
+import theme from "./theme"
 import reportWebVitals from "./reportWebVitals";
+import CssBaseline from "@mui/material/CssBaseline";
+import {ThemeProvider} from "@mui/material/styles";
 import "./css/index.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline/>
+                <App/>
+            </ThemeProvider>
+
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
