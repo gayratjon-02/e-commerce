d52fca88b99619b5ff9e2c7fd58d80327b771f90
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-08-18 01:39:34 +0900

    feat: publish homePage sectional components and add some icons files

--- src/app/screens/HomePage/explore-products.tsx
+++ src/app/screens/HomePage/explore-products.tsx
@@ -1,3 +1,3 @@
 export default function ExploreProducts() {
-    return <div>CustomerService</div>
+  return <div>ExploreProducts</div>;
 }
--- src/app/screens/HomePage/index.tsx
+++ src/app/screens/HomePage/index.tsx
@@ -1,3 +1,29 @@
+import Divider from "../../components/divider";
+import CategoryMain from "./category-main";
+import FlashSales from "./flash-sales";
+import CategoryList from "./category-list";
+import BestSellingProducts from "./best-selling-products";
+import Advertaisment from "./advertaisment";
+import ExploreProducts from "./explore-products";
+import NewArrivalProducts from "./new-arrival-products";
+import CustomerService from "./customer-service";
+import { Container } from "@mui/material";
+import "../../../css/home.css";
+
 export default function HomePage() {
-  return <div>Home Page</div>;
+  return (
+    <div className="home-page">
+      <Container className="home-container">
+        <CategoryMain />
+        <FlashSales />
+        <Divider width="2" height="1" bg="#d9d9d9" />
+        <CategoryList />
+        <BestSellingProducts />
+        <Advertaisment />
+        <ExploreProducts />
+        <NewArrivalProducts />
+        <CustomerService />
+      </Container>
+    </div>
+  );
 }
