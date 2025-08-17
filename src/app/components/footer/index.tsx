47ee930327839a06f9e4a04ec5fbe4301cfaa850
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-08-18 00:46:02 +0900

    feat: publishing footer screen component

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,7 +1,15 @@
 import React from "react";
-import { Box, Container, Stack } from "@mui/material";
+import {
+  Box,
+  Container,
+  InputAdornment,
+  Stack,
+  TextField,
+} from "@mui/material";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import SendIcon from "@mui/icons-material/Send";
+import FacebookIcon from "@mui/icons-material/Facebook";
 
 const Footers = styled.div`
   width: 100%;
@@ -15,75 +23,166 @@ export default function Footer() {
   const authMember = null;
 
   return (
-    <Footers>
-      <Container>
-        <Stack flexDirection={"row"} sx={{ mt: "94px" }}>
-          <Stack flexDirection={"column"} style={{ width: "340px" }}>
-            <Box>
-              <img width={"100px"} src={"/icons/burak.svg"} />
-            </Box>
-            <Box className={"foot-desc-txt"}>
-              Focusing on the gourmet Turkish breakfast as well as the youth
-              society, CZN Burak Gurme aims to bring Turkish cuisine back. CZN
-              Burak Gurme creates an illusion with its cuisine.
-            </Box>
-            <Box className="sns-context">
-              <img src={"/icons/facebook.svg"} />
-              <img src={"/icons/twitter.svg"} />
-              <img src={"/icons/instagram.svg"} />
-              <img src={"/icons/youtube.svg"} />
-            </Box>
-          </Stack>
-          <Stack sx={{ ml: "288px" }} flexDirection={"row"}>
-            <Stack>
-              <Box>
-                <Box className={"foot-category-title"}>Bo'limlar</Box>
-                <Box className={"foot-category-link"}>
-                  <Link to="/">Home</Link>
-                  <Link to="/products">Products</Link>
-                  {authMember && <Link to="/orders">Orders</Link>}
-                  <Link to="/help">Help</Link>
-                </Box>
-              </Box>
-            </Stack>
-            <Stack sx={{ ml: "100px" }}>
-              <Box>
-                <Box className={"foot-category-title"}>Find us</Box>
-                <Box
-                  flexDirection={"column"}
-                  sx={{ mt: "20px" }}
-                  className={"foot-category-link"}
-                  justifyContent={"space-between"}
-                >
-                  <Box flexDirection={"row"} className={"find-us"}>
-                    <span>L.</span>
-                    <div>Downtown, Dubai</div>
-                  </Box>
-                  <Box className={"find-us"}>
-                    <span>P.</span>
-                    <div>+971 4 554 7777</div>
-                  </Box>
-                  <Box className={"find-us"}>
-                    <span>E.</span>
-                    <div>devexuz@gmail.com</div>
-                  </Box>
-                  <Box className={"find-us"}>
-                    <span>H.</span>
-                    <div>Visit 24 hours</div>
-                  </Box>
-                </Box>
-              </Box>
-            </Stack>
-          </Stack>
-        </Stack>
+    <div className="footer">
+      <Container className="footer-container">
         <Stack
-          style={{ border: "1px solid #C5C8C9", width: "100%", opacity: "0.2" }}
-          sx={{ mt: "80px" }}
-        ></Stack>
-        <Stack className={"copyright-txt"}>
-          © Copyright Devex Global, All rights reserved.
+          className="footer-wrapper"
+          flexDirection={"row"}
+          justifyContent={"space-between"}
+        >
+          <Stack
+            className="exclusive-box"
+            flexDirection={"column"}
+            justifyContent={"space-between"}
+          >
+            <Box>
+              <span>Exclusive</span>
+            </Box>
+            <Box>
+              <div>Subscribe</div>
+            </Box>
+            <Box>
+              <p>Get 10% off your first order</p>
+            </Box>
+            <Box>
+              <TextField
+                className="exclusive-inp"
+                variant="standard"
+                id="input-with-icon-textfield"
+                placeholder="Enter your email"
+                slotProps={{
+                  input: {
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <SendIcon className="send-icon" />
+                      </InputAdornment>
+                    ),
+                  },
+                }}
+              />
+            </Box>
+          </Stack>
+          <Stack
+            sx={{ width: "180px" }}
+            className="exclusive-box"
+            flexDirection={"column"}
+            justifyContent={"space-between"}
+          >
+            <Box>
+              <span>Support</span>
+            </Box>
+            <Box>
+              <p>111 Bijoy sarani, Dhaka, DH 1515, Bangladesh.</p>
+            </Box>
+            <Box>
+              <p>exclusive@gmail.com</p>
+              <p>+82-10-8336-2002</p>
+            </Box>
+          </Stack>
+
+          <Stack
+            sx={{ width: "180px", height: "auto" }}
+            className="exclusive-box"
+            flexDirection={"column"}
+            justifyContent={"space-between"}
+          >
+            <Box>
+              <span>Account</span>
+            </Box>
+            <Box>
+              <p>My Account</p>
+              <p>Login / Register</p>
+              <p>Cart</p>
+              <p>Wishlist</p>
+              <p>Shop</p>
+            </Box>
+          </Stack>
+          <Stack
+            sx={{ width: "180px", height: "auto" }}
+            className="exclusive-box"
+            flexDirection={"column"}
+            justifyContent={"space-between"}
+          >
+            <Box>
+              <span>Quick Link</span>
+            </Box>
+            <Box>
+              <p>Privacy Policy</p>
+              <p>Terms Of Use</p>
+              <p>FAQ</p>
+              <p>Contact</p>
+            </Box>
+          </Stack>
+
+          <Stack className="download-box">
+            <Box>
+              <span>Download App</span>
+            </Box>
+
+            <Box>
+              <p>Save $3 with App New User Only</p>
+            </Box>
+
+            <Stack
+              className="download-links"
+              flexDirection={"row"}
+              justifyContent={"space-between"}
+              sx={{
+                width: "198px",
+                height: "84px",
+              }}
+            >
+              <Box>
+                <img
+                  className="qr-code-img"
+                  src="/icons/Qrcode.png"
+                  alt="qrcode"
+                />
+              </Box>
+              <Stack
+                className="app-play-store"
+                flexDirection={"column"}
+                justifyContent={"space-between"}
+              >
+                <img
+                  src="/icons/play-market.png"
+                  className="play-market"
+                  alt="play-market"
+                />
+                <img
+                  src="/icons/app-store.png"
+                  className="app-store"
+                  alt="app-store"
+                />
+              </Stack>
+            </Stack>
+
+            <Stack
+              className="social-links"
+              flexDirection={"row"}
+              justifyContent={"space-between"}
+              sx={{ marginTop: "24px" }}
+            >
+              <img src="/icons/Icon-Facebook.svg" alt="" />
+              <img src="/icons/Icon-Linkedin.svg" alt="" />
+              <img src="/icons/instagram-icon.svg" alt="" />
+              <img src="/icons/Icon-Linkedin.svg" alt="" />
+            </Stack>
+          </Stack>
         </Stack>
       </Container>
-    </Footers>
+      <Stack className="footer-bott" flexDirection={"column"}>
+        <div className="footerLine"></div>
+        <Stack
+          flexDirection={"row"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          className="copywrite-text-box"
+        >
+          <img src="icons/copywriter.svg" alt="copywriter" />
+          <p>Copyright Rimel 2022. All right reserved</p>
+        </Stack>
+      </Stack>
+    </div>
   );
 }
