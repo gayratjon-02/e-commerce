b74252b0ffcea15931cc08df38a4ed2c3fe94636
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-08-19 18:18:06 +0900

    fix: fix navbar padding problem

--- src/app/screens/HomePage/category-main.tsx
+++ src/app/screens/HomePage/category-main.tsx
@@ -1,3 +1,12 @@
+import { Box, Container, Stack } from "@mui/material";
+
 export default function CategoryMain() {
-    return <div>CategoryMain</div>
+  return (
+    <div className="category-main">
+      <Container className="category-container">
+        <Box className="category-left">ss</Box>
+        <Box className="category-right"></Box>
+      </Container>
+    </div>
+  );
 }
