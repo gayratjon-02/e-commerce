7715a6c58561087f69dc926a14039bef82c22450
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-09-12 18:16:13 +0500

    feat: publishing loginPage screen component

---
+++ src/app/screens/Login/index.tsx
@@ -0,0 +1,45 @@
+import { Button, Container, Stack } from "@mui/material";
+import GoogleIcon from "@mui/icons-material/Google";
+import "../../../css/login.css";
+import { NavLink } from "react-router-dom";
+
+export default function Signup() {
+  return (
+    <Container className="signup-container">
+      <Stack className="signup-main" flexDirection={"row"}>
+        <Stack className="signup-left">
+          <img src="/img/phone-sign.png" alt="" />
+        </Stack>
+        <Stack className="signup-right">
+          <h1>Create an account</h1>
+          <p>Enter your details below</p>
+
+          <form action="#">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email or Phone Number"
+            />
+            <input type="password" placeholder="Password" />
+
+            <Button className="signup-butt">Login</Button>
+            <Button className="signup-butt2">
+              <img src="/icons/Icon-Google.png" alt="" />
+              <NavLink to={"#"}>Sign up with Google</NavLink>
+            </Button>
+
+            <Stack
+              className="have-account"
+              flexDirection={"row"}
+              justifyContent={"center"}
+              alignItems={"center"}
+            >
+              <span>Forget Password</span>
+            </Stack>
+          </form>
+        </Stack>
+      </Stack>
+    </Container>
+  );
+}
